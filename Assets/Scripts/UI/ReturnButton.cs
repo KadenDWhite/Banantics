22502339cee41847fab68e87c294cbb111079266
cafeyan7 <bryanarevalo205@gmail.com>
2026-07-31 16:24:05 -0400

    Return Button Progress
    Currently trying to get the return button to run and work smoothly.

--- Assets/Scripts/UI/ReturnButton.cs
+++ Assets/Scripts/UI/ReturnButton.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReturnButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string titleSceneName = "Title";
 
-    // Update is called once per frame
-    void Update()
+    public void ReturnToTitle()
     {
-        
+        SceneManager.LoadScene(titleSceneName);
     }
 }
